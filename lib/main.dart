833dfb2d698f39cdac7498bd9540d5a328c186e7
Tobias Junsten <tobias@junsten.se>
2019-10-22 12:53:23 +0200

    SG-4: added seagull icon

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,10 @@ import 'package:seagull/ui/pages/login_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Seagull',
       theme: ThemeData(
           scaffoldBackgroundColor: const Color(0xFFE6E6E6),
           primaryColor: Colors.black,
@@ -19,15 +18,12 @@ class MyApp extends StatelessWidget {
               buttonColor: RED,
               shape: RoundedRectangleBorder(),
               textTheme: ButtonTextTheme.primary)),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -14,108 +14,121 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
                 children: <Widget>[
-                  Text('Username / Email'),
                   Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: TextField(
-                      decoration: new InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 20.0, horizontal: 10),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                            borderRadius: BorderRadius.circular(12),
+                    padding: const EdgeInsets.only(top: 40, bottom: 20),
+                    child: Image(
+                        image: AssetImage(
+                            'assets/graphics/seagull_icon_gray.png')),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Username / Email'),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: TextField(
+                            decoration: new InputDecoration(
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 20.0, horizontal: 10),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white),
                           ),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                            borderRadius: BorderRadius.circular(12),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Password'),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: TextField(
+                            obscureText: true,
+                            decoration: new InputDecoration(
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 20.0, horizontal: 10),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white),
                           ),
-                          filled: true,
-                          fillColor: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Use your ',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        InkWell(
+                            child: new Text(
+                              'myAbilia',
+                              style: TextStyle(
+                                  color: RED,
+                                  decoration: TextDecoration.underline,
+                                  decorationColor: RED,
+                                  fontSize: 16),
+                            ),
+                            onTap: () =>
+                                launch('https://myabilia.com/user-create')),
+                        Text(
+                          ' account to login',
+                          style: TextStyle(fontSize: 16),
+                        )
+                      ],
                     ),
                   ),
                 ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Password'),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: new InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 20.0, horizontal: 10),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          filled: true,
-                          fillColor: Colors.white),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 70,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(12.0),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'Use your ',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  InkWell(
-                      child: new Text(
-                        'myAbilia',
+                    onPressed: onLogin,
+                    child: Text('Login',
                         style: TextStyle(
-                            color: RED,
-                            decoration: TextDecoration.underline,
-                            decorationColor: RED,
-                            fontSize: 16),
-                      ),
-                      onTap: () => launch('https://myabilia.com/user-create')),
-                  Text(
-                    ' account to login',
-                    style: TextStyle(fontSize: 16),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: SizedBox(
-                width: double.infinity,
-                height: 70,
-                child: RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(12.0),
+                            fontSize: 20, fontWeight: FontWeight.w400)),
                   ),
-                  onPressed: onLogin,
-                  child: Text('Login',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
