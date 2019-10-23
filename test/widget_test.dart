5e312d7b91a0385a6a1981e336c5cc7996905338
Tobias Junsten <tobias@junsten.se>
2019-10-23 12:41:16 +0200

    SG-4: Cleaned the test to do one thing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:seagull/main.dart';
 
@@ -6,8 +5,6 @@ void main() {
   testWidgets("Widget", (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
     await tester.pumpAndSettle();
-    var findByText = find.byType(Text);
-    expect(findByText.evaluate().isEmpty, false);
     expect(find.text('Password'), findsOneWidget);
   });
 }
