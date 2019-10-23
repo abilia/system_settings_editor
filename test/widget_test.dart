e0465ef2d2d6565e74650d27fcf5cbcdfb93c014
Tobias Junsten <tobias@junsten.se>
2019-10-23 12:39:51 +0200

    SG-4: removed unnecessary test code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,26 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:seagull/i18n/app_localizations.dart';
 import 'package:seagull/main.dart';
-
-Widget makeTestableWidget({Widget child}) {
-  return MediaQuery(
-    data: MediaQueryData(),
-    child: MaterialApp(
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      home: child,
-    ),
-  );
-}
 
 void main() {
   testWidgets("Widget", (WidgetTester tester) async {
-    await tester.pumpWidget(makeTestableWidget(child: MyApp()));
+    await tester.pumpWidget(MyApp());
     await tester.pumpAndSettle();
     var findByText = find.byType(Text);
     expect(findByText.evaluate().isEmpty, false);
