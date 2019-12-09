49ff9201e6897b700c6b5630c3aa2a0787a3fade
Tobias Junsten <tobias@junsten.se>
2019-12-09 13:51:21 +0100

    SG-21: get activities on push

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
 
