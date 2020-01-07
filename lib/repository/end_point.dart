59a569c57576ab82be55df41d0aee13fdfa14497
Jonas Bornold <jonas.bornold@abilia.se>
2020-01-07 13:47:43 +0100

    fixing build error on android when using firebase_messaging >6.0.3

--- lib/repository/end_point.dart
+++ lib/repository/end_point.dart
@@ -6,7 +6,6 @@ const Map<String, String> backEndEnviorments = {
   'T1': T1,
   'Whale': WHALE,
   'Prod': PROD,
-  'Local': LOCAL
 };
 
 String thumbImageUrl(String baseUrl, int userId, String imageFileId,
