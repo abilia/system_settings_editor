42fab5e38c66b41f6a7d81ba4abae7214bd1f4ee
Tobias Junsten <tobias@junsten.se>
2020-01-07 08:41:31 +0100

    SG-21: removed temporary background handling

--- lib/background/background.dart
+++ lib/background/background.dart
@@ -1,24 +1,4 @@
-import 'package:http/http.dart';
-import 'package:seagull/db/activities_db.dart';
-import 'package:seagull/db/baseurl_db.dart';
-import 'package:seagull/db/token_db.dart';
-import 'package:seagull/db/user_db.dart';
-import 'package:seagull/repository/activities_repository.dart';
-
 Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
   print('Running myBackgroundMessageHandler');
-  final userDb = UserDb();
-  final baseUrlDb = BaseUrlDb();
-  final baseUrl = await baseUrlDb.getBaseUrl();
-  final user = await userDb.getUser();
-  final tokenDb = TokenDb();
-  final token = await tokenDb.getToken();
-  final activityRepository = ActivityRepository(
-      baseUrl: baseUrl,
-      client: Client(),
-      activitiesDb: ActivityDb(),
-      userId: user.id,
-      authToken: token);
-  final activities = await activityRepository.loadActivities();
-  print('Got activities: ${activities.length}');
+  // TODO replace with alarm scheduling
 }
