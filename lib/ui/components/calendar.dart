f2c7f38c1b2ddaf42c1c52a21598ee0ef4455151
Tobias Junsten <tobias@junsten.se>
2020-02-05 11:30:42 +0100

    SG-83: post merge fix

--- lib/ui/components/calendar.dart
+++ lib/ui/components/calendar.dart
@@ -2,12 +2,11 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:intl/intl.dart';
 
 import 'package:seagull/bloc/all.dart';
-import 'package:seagull/i18n/app_localizations.dart';
 import 'package:seagull/ui/components/all.dart';
 import 'package:seagull/ui/components/calendar/all.dart';
+import 'package:seagull/ui/components/calendar/day_app_bar.dart';
 import 'package:seagull/ui/pages/all.dart';
 import 'package:seagull/ui/theme.dart';
 import 'package:seagull/utils/all.dart';
@@ -165,62 +164,26 @@ class _CalendarState extends State<Calendar> with WidgetsBindingObserver {
   }
 
   PreferredSize buildAppBar(DateTime pickedDay) {
-    final langCode = Locale.cachedLocale.languageCode;
+    final leftAction = ActionButton(
+      child: Icon(
+        AbiliaIcons.return_to_previous_page,
+        size: 32,
+      ),
+      onPressed: () => _dayPickerBloc.add(PreviousDay()),
+    );
+    final rightAction = ActionButton(
+      child: Icon(
+        AbiliaIcons.go_to_next_page,
+        size: 32,
+      ),
+      onPressed: () => _dayPickerBloc.add(NextDay()),
+    );
     return PreferredSize(
       preferredSize: Size.fromHeight(68),
-      child: Builder(
-        builder: (context) {
-          final themeData = Theme.of(context);
-          return AppBar(
-            brightness: getThemeAppBarBrightness()[pickedDay.weekday],
-            elevation: 0.0,
-            automaticallyImplyLeading: false,
-            flexibleSpace: SafeArea(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: ActionButton(
-                      child: Icon(
-                        AbiliaIcons.return_to_previous_page,
-                        size: 32,
-                      ),
-                      onPressed: () => _dayPickerBloc.add(PreviousDay()),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          DateFormat('EEEE, d MMM', langCode).format(pickedDay),
-                          style: themeData.textTheme.title,
-                        ),
-                        Text(
-                          '${Translator.of(context).translate.week} ${pickedDay.getWeekNumber()}',
-                          style: themeData.textTheme.subhead,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 16.0),
-                    child: ActionButton(
-                      child: Icon(
-                        AbiliaIcons.go_to_next_page,
-                        size: 32,
-                      ),
-                      onPressed: () => _dayPickerBloc.add(NextDay()),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
+      child: DayAppBar(
+        day: pickedDay,
+        leftAction: leftAction,
+        rightAction: rightAction,
       ),
     );
   }
