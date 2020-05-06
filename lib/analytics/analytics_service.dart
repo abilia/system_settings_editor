262d30eb58f98e53d96124adb02223e361db5756
Tobias Junsten <tobias@junsten.se>
2020-05-06 14:57:51 +0200

    SGC-163: added appcenter analytics

--- lib/analytics/analytics_service.dart
+++ lib/analytics/analytics_service.dart
@@ -1,3 +1,4 @@
+import 'package:appcenter_analytics/appcenter_analytics.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:seagull/models/activity.dart';
@@ -16,17 +17,20 @@ class AnalyticsService {
   }
 
   static Future<void> sendActivityCreatedEvent(Activity activity) async {
+    final params = <String, dynamic>{
+      'image': activity.hasImage,
+      'title': activity.title?.isNotEmpty ?? false,
+      'fullDay': activity.fullDay,
+      'checkable': activity.checkable,
+      'removeAfter': activity.removeAfter,
+      'alarm': activity.alarm.toInt,
+      'recurring': activity.recurrance.toString(),
+    };
     await analytics.logEvent(
       name: 'activity_created',
-      parameters: <String, dynamic>{
-        'image': activity.hasImage,
-        'title': activity.title?.isNotEmpty ?? false,
-        'fullDay': activity.fullDay,
-        'checkable': activity.checkable,
-        'removeAfter': activity.removeAfter,
-        'alarm': activity.alarm.toInt,
-        'recurring': activity.recurrance.toString(),
-      },
+      parameters: params,
     );
+    await AppCenterAnalytics.trackEvent(
+        "activity_created", params.map((k, v) => MapEntry(k, v.toString())));
   }
 }
