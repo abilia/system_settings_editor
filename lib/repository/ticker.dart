bfdb644210310da40f71bb0dc35720e18494686c
Jonas Bornold <jonas.bornold@abilia.se>
2020-06-02 15:43:51 +0200

    SGC-241 fixing ticker stream not being broadcast

--- lib/repository/ticker.dart
+++ lib/repository/ticker.dart
@@ -4,12 +4,12 @@ import 'package:seagull/utils/all.dart';
 
 class Ticker {
   final DateTime initialTime;
-  final Stream<DateTime> _stream;
+  final Stream<DateTime> stream;
   Ticker({DateTime initialTime, Stream<DateTime> stream})
       : initialTime = (initialTime ?? DateTime.now()).onlyMinutes(),
-        _stream = stream ??
-            Stream.periodic(Duration(seconds: 1), (_) => DateTime.now())
-                .where((dateTime) => dateTime.second == 0)
-                .map((d) => d.onlyMinutes());
-  Stream<DateTime> get stream => _stream.asBroadcastStream();
+        stream = (stream ??
+                Stream.periodic(Duration(seconds: 1), (_) => DateTime.now())
+                    .where((dateTime) => dateTime.second == 0)
+                    .map((d) => d.onlyMinutes()))
+            .asBroadcastStream();
 }
