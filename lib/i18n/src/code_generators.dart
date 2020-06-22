05cdf53d822dc86c563c0236e74c08e5e2aec65b
Jonas Bornold <jonas.bornold@abilia.se>
2020-06-22 12:35:05 +0200

    SGC-251 Fixing typo

--- lib/i18n/src/code_generators.dart
+++ lib/i18n/src/code_generators.dart
@@ -1,5 +1,5 @@
 import 'package:build/build.dart';
 import 'package:seagull/i18n/src/translation_builder.dart';
 
-Builder translatoionBuilder(BuilderOptions options) =>
+Builder translationBuilder(BuilderOptions options) =>
     TranslationBuilder(options);
