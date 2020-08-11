93963da14bf12e5720fd9d9fa4e79c00505727af
Jonas Bornold <jonas.bornold@abilia.se>
2020-08-11 17:04:38 +0200

    SGC-251 English is now fallback when translation is missing

--- lib/i18n/translations.g.dart
+++ lib/i18n/translations.g.dart
@@ -13,446 +13,185 @@ class Locales {
 }
 
 abstract class Translated {
-  final String userName = 'n/a';
-
-  final String password = 'n/a';
-
-  final String infoText1 = 'n/a';
-
-  final String infoText2 = 'n/a';
-
-  final String login = 'n/a';
-
-  final String logout = 'n/a';
-
-  final String menu = 'n/a';
-
-  final String close = 'n/a';
-
-  final String wrongCredentials = 'n/a';
-
-  final String now = 'n/a';
-
-  final String fullDay = 'n/a';
-
-  final String reminders = 'n/a';
-
-  final String alarm = 'n/a';
-
-  final String endsNow = 'n/a';
-
-  final String startsNow = 'n/a';
-
-  final String noActivities = 'n/a';
-
-  final String timePillarView = 'n/a';
-
-  final String listView = 'n/a';
-
-  final String left = 'n/a';
-
-  final String right = 'n/a';
-
-  final String check = 'n/a';
-
-  final String uncheck = 'n/a';
-
-  final String newActivity = 'n/a';
-
-  final String editActivity = 'n/a';
-
-  final String category = 'n/a';
-
-  final String noColor = 'n/a';
-
-  final String selectAlarmType = 'n/a';
-
-  final String alarmAndVibration = 'n/a';
-
-  final String vibration = 'n/a';
-
-  final String noAlarm = 'n/a';
-
-  final String alarmOnlyAtStartTime = 'n/a';
-
-  final String checkable = 'n/a';
-
-  final String deleteAfter = 'n/a';
-
-  final String availableFor = 'n/a';
-
-  final String activityAvailableFor = 'n/a';
-
-  final String meAndSupportPersons = 'n/a';
-
-  final String onlyMe = 'n/a';
-
-  final String name = 'n/a';
-
-  final String picture = 'n/a';
-
-  final String date = 'n/a';
-
-  final String today = 'n/a';
-
-  final String startTime = 'n/a';
-
-  final String endTime = 'n/a';
-
-  final String noEndTime = 'n/a';
-
-  final String calendarView = 'n/a';
-
-  final String selectPicture = 'n/a';
-
-  final String imageArchive = 'n/a';
-
-  final String myPhotos = 'n/a';
-
-  final String takeNewPhoto = 'n/a';
-
-  final String days = 'n/a';
-
-  final String day = 'n/a';
-
-  final String hours = 'n/a';
-
-  final String hour = 'n/a';
-
-  final String h = 'n/a';
-
-  final String minutes = 'n/a';
-
-  final String minute = 'n/a';
-
-  final String min = 'n/a';
-
-  final String am = 'n/a';
-
-  final String pm = 'n/a';
-
-  final String remove = 'n/a';
-
-  final String deleteActivity = 'n/a';
-
-  final String removePicture = 'n/a';
-
-  final String appyTo = 'n/a';
-
-  final String onlyThisDay = 'n/a';
-
-  final String allDays = 'n/a';
-
-  final String thisDayAndForward = 'n/a';
-
-  final String settings = 'n/a';
-
-  final String checkActivityQuestion = 'n/a';
-
-  final String unCheckActivityQuestion = 'n/a';
-
-  final String inTimePre = 'n/a';
-
-  final String timeAgoPre = 'n/a';
-
-  final String timeAgoPost = 'n/a';
-
-  final String missingTitleOrImageAndStartTime = 'n/a';
-
-  final String missingTitleOrImage = 'n/a';
-
-  final String missingStartTime = 'n/a';
-
-  final String infoType = 'n/a';
-
-  final String selectInfoType = 'n/a';
-
-  final String infoTypeNone = 'n/a';
-
-  final String infoTypeChecklist = 'n/a';
-
-  final String infoTypeNote = 'n/a';
-
-  final String typeSomething = 'n/a';
-
-  final String clear = 'n/a';
-
-  final String timepillarSettings = 'n/a';
-
-  final String activityDuration = 'n/a';
-
-  final String dots = 'n/a';
-
-  final String edge = 'n/a';
-
-  final String preview = 'n/a';
-}
-
-class EN extends Translated {
-  @override
   final String userName = 'Username / Email';
 
-  @override
   final String password = 'Password';
 
-  @override
   final String infoText1 = 'Use your ';
 
-  @override
   final String infoText2 = ' account to Log in';
 
-  @override
   final String login = 'Log in';
 
-  @override
   final String logout = 'Log out';
 
-  @override
   final String menu = 'Menu';
 
-  @override
   final String close = 'Close';
 
-  @override
   final String wrongCredentials =
       'Your username/email or password is not correct. Please, try another one.';
 
-  @override
   final String now = 'now';
 
-  @override
   final String fullDay = 'All day';
 
-  @override
   final String reminders = 'Reminders';
 
-  @override
   final String alarm = 'Alarm';
 
-  @override
   final String endsNow = 'Ends now';
 
-  @override
   final String startsNow = 'Starts now';
 
-  @override
   final String noActivities = 'No activities for today';
 
-  @override
   final String timePillarView = 'Timepillar view';
 
-  @override
   final String listView = 'List view';
 
-  @override
   final String left = 'Left';
 
-  @override
   final String right = 'Right';
 
-  @override
   final String check = 'Check';
 
-  @override
   final String uncheck = 'Uncheck';
 
-  @override
   final String newActivity = 'New activity';
 
-  @override
   final String editActivity = 'Edit activity';
 
-  @override
   final String category = 'Category';
 
-  @override
   final String noColor = 'No color';
 
-  @override
   final String selectAlarmType = 'Select alarm type';
 
-  @override
   final String alarmAndVibration = 'Alarm + vibration';
 
-  @override
   final String vibration = 'Vibration';
 
-  @override
   final String noAlarm = 'No alarm';
 
-  @override
   final String alarmOnlyAtStartTime = 'Alarm only at Start time';
 
-  @override
   final String checkable = 'Checkable';
 
-  @override
   final String deleteAfter = 'Delete after';
 
-  @override
   final String availableFor = 'Available for';
 
-  @override
   final String activityAvailableFor = 'Activity available for';
 
-  @override
   final String meAndSupportPersons = 'Me and support persons';
 
-  @override
   final String onlyMe = 'Only me';
 
-  @override
   final String name = 'Name';
 
-  @override
   final String picture = 'Picture';
 
-  @override
   final String date = 'Date';
 
-  @override
   final String today = 'Today';
 
-  @override
   final String startTime = 'Start time';
 
-  @override
   final String endTime = 'End time';
 
-  @override
   final String noEndTime = 'No end time';
 
-  @override
   final String calendarView = 'Calendar view';
 
-  @override
   final String selectPicture = 'Select picture';
 
-  @override
   final String imageArchive = 'Image archive';
 
-  @override
   final String myPhotos = 'My photos';
 
-  @override
   final String takeNewPhoto = 'Take new photo';
 
-  @override
   final String days = 'days';
 
-  @override
   final String day = 'day';
 
-  @override
   final String hours = 'hours';
 
-  @override
   final String hour = 'hour';
 
-  @override
   final String h = 'h';
 
-  @override
   final String minutes = 'minutes';
 
-  @override
   final String minute = 'minute';
 
-  @override
   final String min = 'min';
 
-  @override
   final String am = 'AM';
 
-  @override
   final String pm = 'PM';
 
-  @override
   final String remove = 'Remove';
 
-  @override
   final String deleteActivity = 'Delete activity?';
 
-  @override
   final String removePicture = 'Remove picture';
 
-  @override
   final String appyTo = 'Apply to';
 
-  @override
   final String onlyThisDay = 'Only this day';
 
-  @override
   final String allDays = 'All days';
 
-  @override
   final String thisDayAndForward = 'This day and forward';
 
-  @override
   final String settings = 'Settings';
 
-  @override
   final String checkActivityQuestion = 'Completed?';
 
-  @override
   final String unCheckActivityQuestion = 'Du you want to uncheck?';
 
-  @override
   final String inTimePre = 'in';
 
-  @override
   final String timeAgoPre = '';
 
-  @override
   final String timeAgoPost = 'ago';
 
-  @override
   final String missingTitleOrImageAndStartTime =
       'Your activity should contain an image or name and start time.';
 
-  @override
   final String missingTitleOrImage =
       'Your activity should contain an image or name.';
 
-  @override
   final String missingStartTime = 'Your activity should contain a start time.';
 
-  @override
   final String infoType = 'Info type';
 
-  @override
   final String selectInfoType = 'Select info type';
 
-  @override
   final String infoTypeNone = '- None -';
 
-  @override
   final String infoTypeChecklist = 'Checklist';
 
-  @override
   final String infoTypeNote = 'Note';
 
-  @override
   final String typeSomething = 'Type something…';
 
-  @override
   final String clear = 'Clear';
 
-  @override
   final String timepillarSettings = 'Timepillar settings';
 
-  @override
   final String activityDuration = 'Activity duration';
 
-  @override
   final String dots = 'Dots';
 
-  @override
   final String edge = 'Edge';
 
-  @override
   final String preview = 'Preview';
 }
+
+class EN extends Translated {}
 
 class SV extends Translated {
   @override
