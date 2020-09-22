d9a7313e720331977e7d6aa44ed7bbbf21bebeb0
Tobias Junsten <tobias@junsten.se>
2020-09-22 16:57:47 +0200

    SGC-275: only use generic with highest revision if same identifier

--- lib/db/generic_db.dart
+++ lib/db/generic_db.dart
@@ -1,8 +1,19 @@
+import 'package:collection/collection.dart';
 import 'package:seagull/models/all.dart';
 import 'package:seagull/db/all.dart';
 
 class GenericDb extends DataDb<Generic> {
   GenericDb(Database database) : super(database);
+
+  Future<Iterable<Generic>> getAllNonDeletedMaxRevision() async {
+    final result = await db.rawQuery(GET_ALL_SQL_NON_DELETED);
+    final genericDataModels = result.map(convertToDataModel);
+    final groupByIdentifier = groupBy<DbModel<Generic>, String>(
+        genericDataModels, (m) => m.model.data.identifier);
+    final maxRevisionPerIdentifier = groupByIdentifier.values.map<DbModel<Generic>>(
+        (idList) => maxBy<DbModel<Generic>, int>(idList, (v) => v.revision));
+    return maxRevisionPerIdentifier.map((data) => data.model);
+  }
 
   @override
   String get tableName => DatabaseRepository.GENERIC_TABLE_NAME;
---
+++ test/db/generic_db_test.dart
@@ -0,0 +1,53 @@
+import 'package:seagull/bloc/all.dart';
+import 'package:seagull/db/all.dart';
+import 'package:seagull/models/all.dart';
+import 'package:test/test.dart';
+
+void main() {
+  Database db;
+  GenericDb genericDb;
+
+  setUp(() async {
+    db = await DatabaseRepository.createInMemoryFfiDb();
+    genericDb = GenericDb(db);
+  });
+
+  test('Several with same identifier should get last revision', () async {
+    final all = await genericDb.getAll();
+    expect(all.length, 0);
+
+    final g1 =
+        memoplannerSetting(true, MemoplannerSettings.displayDeleteButtonKey);
+    final g2 =
+        memoplannerSetting(false, MemoplannerSettings.displayDeleteButtonKey);
+    await genericDb.insert([g1.wrapWithDbModel(revision: 1)]);
+    await genericDb.insert([g2.wrapWithDbModel(revision: 2)]);
+
+    final allSettings = await genericDb.getAllNonDeleted();
+    expect(allSettings.length, 2);
+
+    final unique = await genericDb.getAllNonDeletedMaxRevision();
+    expect(unique.length, 1);
+    expect(unique.first.data.identifier,
+        MemoplannerSettings.displayDeleteButtonKey);
+    final settings = MemoplannerSettings.fromSettingsList(
+        unique.map((e) => e.data as MemoplannerSettingData).toList());
+    expect(settings.displayDeleteButton, false);
+  });
+
+  tearDown(() {
+    DatabaseRepository.clearAll(db);
+  });
+}
+
+Generic<MemoplannerSettingData> memoplannerSetting(
+    bool value, String identifier) {
+  return Generic.createNew<MemoplannerSettingData>(
+    data: MemoplannerSettingData(
+      data: value.toString(),
+      type: 'Boolean',
+      identifier: identifier,
+    ),
+    type: GenericType.memoPlannerSettings,
+  );
+}
