adcbb3b02d682977dafc4387a6616a70f5bf4b91
Jonas Bornold <jonas@bornold.se>
2020-12-04 12:30:49 +0100

    SGC-445 Adding keysigning for playstore and adding aab build for release

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -20,14 +20,16 @@ echo "Installed flutter to `pwd`/flutter"
 # run tests
 flutter test
 
-# build APK
-flutter build apk --release --build-number=$APPCENTER_BUILD_ID --dart-define=release=$release
 
-# if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
-#flutter build appbundle
-
-# copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_/seagull-$buildName.apk
-
-# copy the AAB where AppCenter will find it
-#mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app.aab $_
+if [ "$release" ]
+  then
+    echo "Building APK"
+    flutter build apk --release --build-number=$APPCENTER_BUILD_ID --dart-define=release=$release
+    # copy the APK where AppCenter will find it
+    mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_memoplannergo-$APPCENTER_BUILD_ID-$release.apk
+  else
+    echo "Building AAB"
+    flutter build appbundle --build-number=$APPCENTER_BUILD_ID
+    # copy the AAB where AppCenter will find it
+    mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app-release.aab $_memoplannergo-$APPCENTER_BUILD_ID.aab
+fi
