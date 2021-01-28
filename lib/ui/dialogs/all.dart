f7b4c21d75b0d1b3939efd912fe3b37158fe1200
Jonas Bornold <jonas@bornold.se>
2021-01-28 15:17:45 +0100

    SGC-595 Rework permission_warning.dart

--- lib/ui/dialogs/all.dart
+++ lib/ui/dialogs/all.dart
@@ -2,7 +2,6 @@ export 'view_dialog.dart';
 export 'confirm_activity_action.dart';
 export 'edit_recurrant.dart';
 export 'license_expired.dart';
-export 'warning.dart';
 export 'permission/all.dart';
 export 'small_dialog.dart';
 export 'eye_button_dialog.dart';
--- lib/ui/dialogs/permission/permission_warning.dart
+++ lib/ui/dialogs/permission/permission_warning.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
 import 'package:seagull/ui/all.dart';
 
 class NotificationPermissionOffWarningDialog extends StatelessWidget {
@@ -12,22 +11,33 @@ class NotificationPermissionOffWarningDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final translate = Translator.of(context).translate;
-    return WarningDialog(
-      onOk: () async {
-        await Navigator.of(context).maybePop();
-        await onOk();
-      },
-      icon: const Icon(
-        AbiliaIcons.ir_error,
-        size: hugeIconSize,
-        color: AbiliaColors.orange,
+    return SmallDialog(
+      expanded: true,
+      backNavigationWidget: GreyButton(
+        icon: AbiliaIcons.close_program,
+        text: translate.no,
+        onPressed: Navigator.of(context).maybePop,
       ),
-      heading: Translator.of(context).translate.turnOffNotifications,
-      text: Tts(
-        child: Text(
-          translate.turnOffNotificationsBody,
-          textAlign: TextAlign.center,
+      forwardNavigationWidget: GreenButton(
+        icon: AbiliaIcons.ok,
+        text: translate.yes,
+        onPressed: () async {
+          await Navigator.of(context).maybePop();
+          await onOk();
+        },
+      ),
+      body: _WarningContent(
+        body: Tts(
+          child: Text(
+            translate.turnOffNotificationsBody,
+            style: Theme.of(context)
+                .textTheme
+                .bodyText2
+                .copyWith(color: AbiliaColors.black75),
+            textAlign: TextAlign.center,
+          ),
         ),
+        heading: translate.turnOffNotifications,
       ),
     );
   }
@@ -38,24 +48,40 @@ class NotificationPermissionWarningDialog extends StatelessWidget {
     Key key,
   }) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-    return WarningDialog(
-      icon: const Icon(
-        AbiliaIcons.ir_error,
-        size: hugeIconSize,
-        color: AbiliaColors.orange,
-      ),
-      heading: Translator.of(context).translate.allowNotifications,
-      text: NotificationBodyTextWarning(),
-    );
-  }
+  Widget build(BuildContext context) => SmallDialog(
+        expanded: true,
+        backNavigationWidget: const CloseButton(),
+        body: _WarningContent(
+          heading: Translator.of(context).translate.allowNotifications,
+          body: const NotificationBodyTextWarning(),
+        ),
+      );
+}
+
+class _WarningContent extends StatelessWidget {
+  const _WarningContent({Key key, this.heading, this.body}) : super(key: key);
+  final String heading;
+  final Widget body;
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          const SizedBox(height: 128.0),
+          const Icon(
+            AbiliaIcons.ir_error,
+            size: hugeIconSize,
+            color: AbiliaColors.orange,
+          ),
+          const SizedBox(height: 80.0),
+          Text(heading, style: Theme.of(context).textTheme.headline6),
+          const SizedBox(height: 8.0),
+          body,
+        ],
+      );
 }
 
 @visibleForTesting
 class NotificationBodyTextWarning extends StatelessWidget {
-  const NotificationBodyTextWarning({
-    Key key,
-  }) : super(key: key);
+  const NotificationBodyTextWarning({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +100,10 @@ class NotificationBodyTextWarning extends StatelessWidget {
         text: TextSpan(
           style: b1,
           children: [
-            TextSpan(text: '${translate.allowNotificationsDescription1} '),
+            TextSpan(
+              text: '${translate.allowNotificationsDescription1} ',
+              style: b1,
+            ),
             buildSettingsLinkTextSpan(context),
           ],
         ),
