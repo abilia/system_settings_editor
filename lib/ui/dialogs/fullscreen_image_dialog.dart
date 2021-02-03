f837b90e94dbadc89b2d3c52d4099f4189eb0bb2
Tobias Junsten <tobias@junsten.se>
2021-02-03 17:58:45 +0100

    SGC-538: new fullscreen image dialog and some paddings

---
+++ lib/ui/dialogs/fullscreen_image_dialog.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart' hide CloseButton;
+import 'package:seagull/ui/all.dart';
+
+import 'all.dart';
+
+class FullscreenImageDialog extends StatelessWidget {
+  final String fileId;
+  final String filePath;
+  const FullscreenImageDialog({
+    Key key,
+    @required this.fileId,
+    @required this.filePath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SmallDialog(
+      expanded: true,
+      bodyPadding: EdgeInsets.zero,
+      body: FullScreenImage(
+        backgroundDecoration: whiteNoBorderBoxDecoration.copyWith(
+          borderRadius: BorderRadius.zero,
+        ),
+        fileId: fileId,
+        filePath: filePath,
+      ),
+      backNavigationWidget: CloseButton(),
+    );
+  }
+}
