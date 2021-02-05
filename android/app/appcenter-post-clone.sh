e2b936bbd899b76ef857482f8e5ec87f37055530
Jonas Bornold <jonas@bornold.se>
2021-02-05 14:47:51 +0100

    SGC-606 Setting correct build path for build script

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -23,11 +23,11 @@ flutter test
 echo "Building APK"
 flutter build apk --flavor $flavor --release --build-number=$APPCENTER_BUILD_ID --dart-define=release=$release
 # copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-$flavor-release.apk $_/$flavor-$APPCENTER_BUILD_ID-$release.apk
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/flutter-apk/app-$flavor-release.apk $_/$flavor-$APPCENTER_BUILD_ID-$release.apk
 
 if [ "$APPCENTER_BRANCH" = "release" ]; then
   echo "Building AAB"
   flutter build appbundle --flavor $flavor --build-number=$APPCENTER_BUILD_ID
   # copy the AAB where AppCenter will find it
-  mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/release/app-$flavor-release.aab $_/$flavor-$APPCENTER_BUILD_ID.aab
+  mkdir -p android/app/build/outputs/bundle/; mv build/app/outputs/bundle/${flavor}Release/app-$flavor-release.aab $_/$flavor-$APPCENTER_BUILD_ID.aab
 fi
