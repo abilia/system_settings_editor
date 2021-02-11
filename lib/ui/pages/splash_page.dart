42aec1b49560561addea809441857c37027cc6bd
Jonas Bornold <jonas@bornold.se>
2021-02-11 08:36:49 +0100

    SGC-619 removing flare splash

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,19 +1,8 @@
-import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:seagull/ui/all.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: AbiliaColors.white110,
-      child: FlareActor(
-        'assets/animation/MPGO.flr',
-        alignment: Alignment.center,
-        fit: BoxFit.contain,
-        animation: 'v.2',
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Container(color: AbiliaColors.white110);
 }
