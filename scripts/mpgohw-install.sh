61a52e85e749fece39905b4bccec18428300da22
DanLud <dan.ludvigsson@abilia.se>
2021-02-21 23:04:48 +0100

    SGC-531 created an easy script for install and locking of device

---
+++ scripts/mpgohw-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Simple script for installing Memoplanner and locking the device to memoplanner. 
+# Requires the device to be open to connections through adb, developer mode.
+# Also need adb to be setup on computer running the script.
+
+APK="mp.apk"
+
+echo Starting
+# Add -t with test-only flag.
+adb install $APK
+
+# Sets application as device owner
+adb shell dpm set-device-owner com.abilia.memoplanner/.DeviceAdminReceiver
+# Starts the application after install
+adb shell am start -n com.abilia.memoplanner/.MainActivity
+sleep 5
+# Starts the service which sets the locks for the 
+adb shell am startservice -n com.abilia.memoplanner/.LockService
+
+echo Done
+read input
