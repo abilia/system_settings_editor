35a4c597b32e112c0bec87fe20f295a6488ea72f
DanLud <dan.ludvigsson@abilia.se>
2021-02-22 09:32:40 +0100

    SGC-531 updated from review suggestions

--- scripts/mpgohw-install.sh
+++ scripts/mpgohw-install.sh
@@ -3,6 +3,7 @@
 # Requires the device to be open to connections through adb, developer mode.
 # Also need adb to be setup on computer running the script.
 
+set -e
 APK="mp.apk"
 
 echo Starting
@@ -12,10 +13,8 @@ adb install $APK
 # Sets application as device owner
 adb shell dpm set-device-owner com.abilia.memoplanner/.DeviceAdminReceiver
 # Starts the application after install. Needed for the next step
-adb shell am start -n com.abilia.memoplanner/.MainActivity
-sleep 5
+adb shell am start -W -n com.abilia.memoplanner/.MainActivity
 # Starts the service which locks the device to the application
 adb shell am startservice -n com.abilia.memoplanner/.LockService
 
 echo Done
-read input
