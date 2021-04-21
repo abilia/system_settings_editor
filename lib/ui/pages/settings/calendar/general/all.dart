49acb9369961fc9d870d7518988da4926173acc8
Jonas Bornold <jonas@bornold.se>
2021-04-21 14:13:23 +0200

    SGC-739 Renaming to calendar_general_settings_page.dart

--- lib/ui/pages/settings/calendar/general/all.dart
+++ lib/ui/pages/settings/calendar/general/all.dart
@@ -1,4 +1,4 @@
-export 'calendar_general_settings.dart';
+export 'calendar_general_settings_page.dart';
 export 'categories_settings_tab.dart';
 export 'clock_settings_tab.dart';
 export 'day_color_settings_tab.dart';
