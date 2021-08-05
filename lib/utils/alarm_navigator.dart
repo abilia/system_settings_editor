3946d9c584dac8a702ce332e8002471e996874e3
Jonas Bornold <jonas@bornold.se>
2021-08-05 10:56:01 +0200

    SGC-846 Intercepting pop scope to remove current alarm

--- lib/utils/alarm_navigator.dart
+++ lib/utils/alarm_navigator.dart
@@ -1,25 +1,37 @@
-import 'package:flutter/material.dart';
 import 'package:seagull/bloc/all.dart';
+import 'package:seagull/listener.dart';
+import 'package:seagull/logging.dart';
 import 'package:seagull/models/all.dart';
-import 'package:seagull/ui/pages/all.dart';
+import 'package:seagull/ui/all.dart';
 
 class AlarmNavigator {
-  final alarmRouteObserver = RouteObserver<MaterialPageRoute>();
-  final _alarmRoutesOnStack = <String, AlarmPageRoute>{};
+  final _alarmRoutesOnStack = <String, MaterialPageRoute>{};
+  static final log = Logger((AlarmNavigator).toString());
+
+  Route getFullscreenAlarmRoute({
+    required NotificationAlarm alarm,
+    required Authenticated authenticatedState,
+  }) {
+    log.fine('pushFullscreenAlarm: $alarm');
+    final route = MaterialPageRoute(
+      builder: (_) => AuthenticatedBlocsProvider(
+        authenticatedState: authenticatedState,
+        child: AlarmListeners(child: _alarmPage(alarm)),
+      ),
+    );
+    _alarmRoutesOnStack[alarm.activity.id] = route;
+    return route;
+  }
 
   Future pushAlarm(
     BuildContext context,
     NotificationAlarm alarm,
   ) async {
-    final route = AlarmPageRoute(
-      alarm,
+    log.fine('pushAlarm: $alarm');
+    final route = MaterialPageRoute(
       builder: (_) => CopiedAuthProviders(
         blocContext: context,
-        child: (alarm is NewAlarm)
-            ? NavigatableAlarmPage(alarm: alarm, alarmNavigator: this)
-            : (alarm is NewReminder)
-                ? NavigatableReminderPage(reminder: alarm, alarmNavigator: this)
-                : throw UnsupportedError('$alarm not supported'),
+        child: _alarmPage(alarm),
       ),
       fullscreenDialog: true,
     );
@@ -28,31 +40,31 @@ class AlarmNavigator {
     final routeOnStack = _alarmRoutesOnStack[id];
     final navigator = Navigator.of(context);
     if (routeOnStack != null) {
-      navigator.removeRoute(routeOnStack);
+      log.fine('pushed alarm exists on stack');
+      if (navigator.canPop()) {
+        log.finer('alarm is not root, removes');
+        navigator.removeRoute(routeOnStack);
+      } else {
+        log.finer('alarm is root, replacing');
+        return navigator.pushAndRemoveUntil(route, (_) => false);
+      }
     }
     _alarmRoutesOnStack[id] = route;
     return navigator.push(route);
   }
 
-  AlarmPageRoute? removedFromRoutes(NotificationAlarm alarm) {
+  Widget _alarmPage(NotificationAlarm alarm) => PopAwareAlarmPage(
+        alarm: alarm,
+        alarmNavigator: this,
+        child: (alarm is NewAlarm)
+            ? AlarmPage(alarm: alarm)
+            : (alarm is NewReminder)
+                ? ReminderPage(reminder: alarm)
+                : throw UnsupportedError('$alarm not supported'),
+      );
+
+  MaterialPageRoute? removedFromRoutes(NotificationAlarm alarm) {
+    log.shout('removedFromRoutes: $alarm');
     return _alarmRoutesOnStack.remove(alarm.activity.id);
   }
-}
-
-class AlarmPageRoute<T> extends MaterialPageRoute<T> {
-  final NotificationAlarm alarm;
-  AlarmPageRoute(
-    this.alarm, {
-    required WidgetBuilder builder,
-    RouteSettings? settings,
-    bool maintainState = true,
-    bool fullscreenDialog = false,
-  }) : super(
-          builder: builder,
-          settings: settings,
-          maintainState: maintainState,
-          fullscreenDialog: fullscreenDialog,
-        );
-  @override
-  String get debugLabel => '${super.debugLabel}($alarm)';
 }
