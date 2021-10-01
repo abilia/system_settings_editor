6fd02da13c6bcc1e4e4c488d7460c0b96a8dbb4e
rickard <rickard@mindemia.com>
2021-10-01 13:30:24 +0200

    SGC-345-SGC-986-review fixes

--- lib/ui/components/activity/activity_wizard/available_for_wiz.dart
+++ lib/ui/components/activity/activity_wizard/available_for_wiz.dart
@@ -1,65 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:seagull/bloc/activities/edit_activity/edit_activity_bloc.dart';
-import 'package:seagull/models/activity/activity.dart';
 import 'package:seagull/ui/all.dart';
 
-class AvailableForWiz extends StatefulWidget {
+class AvailableForWiz extends StatelessWidget {
   const AvailableForWiz({Key? key}) : super(key: key);
-
-  @override
-  _AvailableForWizState createState() => _AvailableForWizState();
-}
-
-class _AvailableForWizState extends State<AvailableForWiz> {
-  _AvailableForWizState();
-  late final Activity activity;
-  bool? secret;
-  void _onSelected(bool? value) => setState(() => secret = value);
-
-  @override
-  void initState() {
-    super.initState();
-    secret = BlocProvider.of<EditActivityBloc>(context).state.activity.secret;
-    activity = context.read<EditActivityBloc>().state.activity;
-  }
 
   @override
   Widget build(BuildContext context) {
     final translate = Translator.of(context).translate;
-    return Scaffold(
-      appBar: AbiliaAppBar(
-        iconData: AbiliaIcons.unlock,
-        title: translate.availableFor,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(12.0.s, 24.0.s, 16.0.s, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            RadioField<bool?>(
-              groupValue: secret,
-              onChanged: _onSelected,
-              value: true,
-              leading: const Icon(AbiliaIcons.password_protection),
-              text: Text(translate.onlyMe),
-            ),
-            SizedBox(height: 8.0.s),
-            RadioField<bool?>(
-              groupValue: secret,
-              onChanged: _onSelected,
-              value: false,
-              leading: const Icon(AbiliaIcons.user_group),
-              text: Text(translate.meAndSupportPersons),
-            ),
-          ],
+    return BlocBuilder<EditActivityBloc, EditActivityState>(
+      builder: (context, state) => Scaffold(
+        appBar: AbiliaAppBar(
+          iconData: AbiliaIcons.unlock,
+          title: translate.availableFor,
         ),
-      ),
-      bottomNavigationBar: WizardBottomNavigation(
-        beforeOnNext: () {
-          BlocProvider.of<EditActivityBloc>(context)
-              .add(ReplaceActivity(activity.copyWith(secret: secret)));
-        },
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(12.0.s, 24.0.s, 16.0.s, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              RadioField<bool?>(
+                groupValue: state.activity.secret,
+                onChanged: (value) => context.read<EditActivityBloc>().add(
+                    ReplaceActivity(state.activity.copyWith(secret: value))),
+                value: true,
+                leading: const Icon(AbiliaIcons.password_protection),
+                text: Text(translate.onlyMe),
+              ),
+              SizedBox(height: 8.0.s),
+              RadioField<bool?>(
+                groupValue: state.activity.secret,
+                onChanged: (value) => context.read<EditActivityBloc>().add(
+                    ReplaceActivity(state.activity.copyWith(secret: value))),
+                value: false,
+                leading: const Icon(AbiliaIcons.user_group),
+                text: Text(translate.meAndSupportPersons),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: WizardBottomNavigation(),
       ),
     );
   }
