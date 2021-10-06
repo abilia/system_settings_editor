53392ff571cf3337cb66f234caf52d322dd8f125
rickard <rickard@mindemia.com>
2021-10-06 13:30:06 +0200

    SGC-345-SGC-1004 monthly recurrence needs scroll bars (when end date)

--- lib/ui/components/activity/activity_wizard/recurring_monthly_wiz.dart
+++ lib/ui/components/activity/activity_wizard/recurring_monthly_wiz.dart
@@ -5,29 +5,34 @@ class RecurringMonthlyWiz extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scrollController = ScrollController();
     return Scaffold(
       appBar: AbiliaAppBar(
         title: Translator.of(context).translate.monthly,
         iconData: AbiliaIcons.month,
       ),
-      body: Column(
-        children: [
-          ...[
-            SizedBox(height: 24.s),
-            MonthDays(),
-            SizedBox(height: 16.s),
-            Divider(),
-            SizedBox(height: 24.s),
-            EndDateWidget(),
-          ].map(
-            (w) => w is Divider
-                ? w
-                : Padding(
-                    padding: EdgeInsets.only(left: 12.s, right: 16.s),
-                    child: w,
-                  ),
-          )
-        ],
+      body: VerticalScrollArrows(
+        controller: scrollController,
+        child: ListView(
+          controller: scrollController,
+          children: <Widget>[
+            ...[
+              SizedBox(height: 24.s),
+              MonthDays(),
+              SizedBox(height: 16.s),
+              Divider(),
+              SizedBox(height: 24.s),
+              EndDateWidget(),
+            ].map(
+              (w) => w is Divider
+                  ? w
+                  : Padding(
+                      padding: EdgeInsets.only(left: 12.s, right: 16.s),
+                      child: w,
+                    ),
+            )
+          ],
+        ),
       ),
       bottomNavigationBar: const WizardBottomNavigation(),
     );
