4edda4482bb223f0af7c6fc0a47817e49f8d43a7
rickard <rickard@mindemia.com>
2021-10-21 11:11:09 +0200

    SGC-754 removing unused DateTime check

--- lib/models/generic/generic_data.dart
+++ lib/models/generic/generic_data.dart
@@ -55,9 +55,6 @@ class MemoplannerSettingData<T> extends GenericData {
       case String:
         type = 'String';
         break;
-      case DateTime:
-        type = 'DateTime';
-        break;
       default:
         throw UnimplementedError();
     }
