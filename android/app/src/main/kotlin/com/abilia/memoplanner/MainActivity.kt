dd6021ec779ff4eda1bb2df1890e064fcfed2562
Tobias Junsten <tobias@junsten.se>
2021-10-29 09:10:52 +0200

    SGC-582: removed unused imorts

--- android/app/src/main/kotlin/com/abilia/memoplanner/MainActivity.kt
+++ android/app/src/main/kotlin/com/abilia/memoplanner/MainActivity.kt
@@ -5,19 +5,6 @@ import android.os.Bundle
 import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-import androidx.annotation.NonNull
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
-import android.content.res.Resources
-import android.content.ContentResolver
-import android.content.Intent
-import android.net.Uri
-import android.provider.Settings
-import android.util.Log
-
-import android.provider.Settings.SettingNotFoundException
-import androidx.annotation.RequiresApi
-
 open class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
