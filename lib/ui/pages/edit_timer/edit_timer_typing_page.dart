bad1355b9050b0ed79e965202aea29a2ecc2275b
rickard <rickardeden@gmail.com>
2021-12-10 09:27:15 +0100

    SGC-1162-but-with-cubit

--- lib/ui/pages/edit_timer/edit_timer_typing_page.dart
+++ lib/ui/pages/edit_timer/edit_timer_typing_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:seagull/bloc/all.dart';
+import 'package:seagull/bloc/edit_timer/edit_timer_cubit.dart';
 import 'package:seagull/ui/all.dart';
 
 String _pad0(String s) => s.padLeft(2, '0');
-
-typedef _BottomNavigationBuilder = Widget Function(
-    BuildContext context, Duration? newDuration);
 
 const _textSelection = TextSelection(baseOffset: 0, extentOffset: 2);
 const _emptyPattern = '00';
@@ -22,153 +21,137 @@ class EditTimerByTypingPage extends StatelessWidget {
         title: Translator.of(context).translate.setTime,
         iconData: AbiliaIcons.clock,
       ),
-      body: _TimerInputContent(
-        bottomNavigationBuilder: (context, duration) => BottomNavigation(
-          backNavigationWidget: const CancelButton(),
-          forwardNavigationWidget: duration != null && duration.inMinutes > 0
-              ? SaveButton(
-                  onPressed: () => Navigator.of(context).pop(duration),
-                )
-              : DarkGreyButton(
-                  text: Translator.of(context).translate.save,
-                  icon: AbiliaIcons.ok),
-        ),
-      ),
+      body: _TimerInputContent(),
     );
   }
 }
 
 class _TimerInputContent extends StatefulWidget {
-  final _BottomNavigationBuilder bottomNavigationBuilder;
-
-  const _TimerInputContent({
-    Key? key,
-    required this.bottomNavigationBuilder,
-  }) : super(key: key);
-
   @override
-  _TimeInputContentState createState() => _TimeInputContentState();
+  State<StatefulWidget> createState() {
+    return _TimerInputState();
+  }
 }
 
-class _TimeInputContentState extends State<_TimerInputContent> {
-  final TextEditingController _hourEditingController =
-      TextEditingController(text: _emptyPattern);
-  final TextEditingController _minuteEditingController =
-      TextEditingController(text: _emptyPattern);
-  late final FocusNode _hourFocus;
-  late final FocusNode _minuteFocus;
-
-  @override
-  void initState() {
-    super.initState();
-    _hourFocus = FocusNode()
-      ..addListener(() {
-        if (_hourFocus.hasFocus) {
-          _hourEditingController.selection = _textSelection;
-        }
-      });
-    _minuteFocus = FocusNode()
-      ..requestFocus()
-      ..addListener(() {
-        if (_minuteFocus.hasFocus) {
-          _minuteEditingController.selection = _textSelection;
-        }
-      });
-  }
+class _TimerInputState extends State<_TimerInputContent> {
+  bool _minuteFocus = true;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final translate = Translator.of(context).translate;
-    return Column(
-      children: [
-        Theme(
-          data: theme.copyWith(
-              textSelectionTheme: theme.textSelectionTheme.copyWith(
-                selectionColor: Colors.transparent,
-              ),
-              textTheme: theme.textTheme
-                  .copyWith(subtitle1: abiliaTextTheme.headline4)),
-          child: Padding(
-            padding: EdgeInsets.only(left: 42.s, top: 64.s, right: 42.s),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                _TimeTextField(
-                    key: TestKey.hours,
-                    header: translate.hoursCap,
-                    editController: _hourEditingController,
-                    focusNode: _hourFocus,
-                    onChanged: (value) => _onTimeChanged(value),
-                    inputFormatters: [
-                      FilteringTextInputFormatter.digitsOnly,
-                      _EmptyInputFormatter(),
-                      _LeadingZeroInputFormatter(),
-                      LengthLimitingTextInputFormatter(2),
-                      _HourInputFormatter()
-                    ]),
-                Padding(
-                  padding: EdgeInsets.all(16.s),
-                  child: Text(
-                    ':',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline5
-                        ?.copyWith(color: AbiliaColors.black100),
+    return BlocProvider<EditTimerCubit>(
+      create: (context) => EditTimerCubit(0, 0),
+      child: BlocSelector<EditTimerCubit, EditTimerState, Duration>(
+        selector: (state) => state.duration,
+        builder: (context, duration) {
+          return Column(
+            children: [
+              Theme(
+                data: theme.copyWith(
+                    textSelectionTheme: theme.textSelectionTheme.copyWith(
+                      selectionColor: Colors.transparent,
+                    ),
+                    textTheme: theme.textTheme
+                        .copyWith(subtitle1: abiliaTextTheme.headline4)),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 42.s, top: 64.s, right: 42.s),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      _TimeTextField(
+                          key: TestKey.hours,
+                          header: translate.hoursCap,
+                          text: _pad0(duration.inHours.toString()),
+                          focus: !_minuteFocus,
+                          onChanged: (hours) {
+                            context
+                                .read<EditTimerCubit>()
+                                .updateDuration(hours: int.tryParse(hours));
+                            _minuteFocus = false;
+                          },
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly,
+                            _EmptyInputFormatter(),
+                            _LeadingZeroInputFormatter(),
+                            LengthLimitingTextInputFormatter(2),
+                            _HourInputFormatter()
+                          ]),
+                      Padding(
+                        padding: EdgeInsets.all(16.s),
+                        child: Text(
+                          ':',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5
+                              ?.copyWith(color: AbiliaColors.black100),
+                        ),
+                      ),
+                      _TimeTextField(
+                          key: TestKey.minutes,
+                          header: translate.minutesCap,
+                          text: (duration.inMinutes % Duration.minutesPerHour)
+                              .toString(),
+                          focus: _minuteFocus,
+                          onChanged: (minutes) {
+                            context
+                                .read<EditTimerCubit>()
+                                .updateDuration(minutes: int.tryParse(minutes));
+                            _minuteFocus = true;
+                          },
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly,
+                            _EmptyInputFormatter(),
+                            _LeadingZeroInputFormatter(),
+                            LengthLimitingTextInputFormatter(2),
+                            _MinuteInputFormatter()
+                          ]),
+                    ],
                   ),
                 ),
-                _TimeTextField(
-                    key: TestKey.minutes,
-                    header: translate.minutesCap,
-                    editController: _minuteEditingController,
-                    focusNode: _minuteFocus,
-                    onChanged: (value) => _onTimeChanged(value),
-                    inputFormatters: [
-                      FilteringTextInputFormatter.digitsOnly,
-                      _EmptyInputFormatter(),
-                      _LeadingZeroInputFormatter(),
-                      LengthLimitingTextInputFormatter(2),
-                      _MinuteInputFormatter()
-                    ]),
-              ],
-            ),
-          ),
-        ),
-        const Spacer(),
-        widget.bottomNavigationBuilder(
-          context,
-          Duration(
-              hours: int.tryParse(_hourEditingController.text) ?? 0,
-              minutes: int.tryParse(_minuteEditingController.text) ?? 0),
-        ),
-      ],
+              ),
+              const Spacer(),
+              BottomNavigation(
+                backNavigationWidget: const CancelButton(),
+                forwardNavigationWidget: duration.inMinutes > 0
+                    ? SaveButton(
+                        onPressed: () => Navigator.of(context).pop(duration),
+                      )
+                    : DarkGreyButton(
+                        text: Translator.of(context).translate.save,
+                        icon: AbiliaIcons.ok),
+              ),
+            ],
+          );
+        },
+      ),
     );
-  }
-
-  _onTimeChanged(String value) {
-    setState(() {});
   }
 }
 
 class _TimeTextField extends StatelessWidget {
-  final TextEditingController editController;
   final ValueChanged<String> onChanged;
   final List<TextInputFormatter> inputFormatters;
   final String header;
-  final FocusNode focusNode;
+  final FocusNode focusNode = FocusNode();
+  final String text;
+  final bool focus;
 
-  const _TimeTextField(
+  _TimeTextField(
       {Key? key,
+      required this.text,
+      required this.focus,
       required this.header,
-      required this.editController,
       required this.onChanged,
-      required this.inputFormatters,
-      required this.focusNode})
+      required this.inputFormatters})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    if (focus) {
+      focusNode.requestFocus();
+    }
     return Expanded(
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -182,7 +165,8 @@ class _TimeTextField extends StatelessWidget {
             enableInteractiveSelection: false,
             keyboardType: TextInputType.number,
             showCursor: false,
-            controller: editController,
+            controller: TextEditingController(text: _pad0(text))
+              ..selection = _textSelection,
             onChanged: (value) => onChanged.call(value),
             textInputAction: TextInputAction.done,
             inputFormatters: inputFormatters,
