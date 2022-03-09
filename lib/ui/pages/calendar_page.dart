163bdae932ffb7a7a915526e0ddf9d7b629622a6
Tobias Junsten <tobias@junsten.se>
2022-03-09 11:33:18 +0100

    SGC-1204: added white background to calendar

--- lib/ui/pages/calendar_page.dart
+++ lib/ui/pages/calendar_page.dart
@@ -5,32 +5,36 @@ class CalendarPage extends StatelessWidget {
   const CalendarPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<MemoplannerSettingBloc, MemoplannerSettingsState>(
-      buildWhen: (old, fresh) =>
-          old.runtimeType != fresh.runtimeType ||
-          old.calendarCount != fresh.calendarCount ||
-          old.displayBottomBar != fresh.displayBottomBar,
-      builder: (context, settingsState) => Listener(
-        onPointerDown: Config.isMP
-            ? context.read<InactivityCubit>().activityDetected
-            : null,
-        child: DefaultTabController(
-          initialIndex: 0,
-          length: settingsState.calendarCount,
-          child: Scaffold(
-            bottomNavigationBar: settingsState is MemoplannerSettingsLoaded &&
-                    settingsState.displayBottomBar
-                ? const CalendarBottomBar()
-                : null,
-            body: TabBarView(
-              physics: const NeverScrollableScrollPhysics(),
-              children: [
-                const DayCalendar(),
-                if (settingsState.displayWeekCalendar) const WeekCalendarTab(),
-                if (settingsState.displayMonthCalendar)
-                  const MonthCalendarTab(),
-                if (settingsState.displayMenu) const MenuPage(),
-              ],
+    return Theme(
+      data: abiliaTheme.copyWith(scaffoldBackgroundColor: AbiliaColors.white),
+      child: BlocBuilder<MemoplannerSettingBloc, MemoplannerSettingsState>(
+        buildWhen: (old, fresh) =>
+            old.runtimeType != fresh.runtimeType ||
+            old.calendarCount != fresh.calendarCount ||
+            old.displayBottomBar != fresh.displayBottomBar,
+        builder: (context, settingsState) => Listener(
+          onPointerDown: Config.isMP
+              ? context.read<InactivityCubit>().activityDetected
+              : null,
+          child: DefaultTabController(
+            initialIndex: 0,
+            length: settingsState.calendarCount,
+            child: Scaffold(
+              bottomNavigationBar: settingsState is MemoplannerSettingsLoaded &&
+                      settingsState.displayBottomBar
+                  ? const CalendarBottomBar()
+                  : null,
+              body: TabBarView(
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  const DayCalendar(),
+                  if (settingsState.displayWeekCalendar)
+                    const WeekCalendarTab(),
+                  if (settingsState.displayMonthCalendar)
+                    const MonthCalendarTab(),
+                  if (settingsState.displayMenu) const MenuPage(),
+                ],
+              ),
             ),
           ),
         ),
