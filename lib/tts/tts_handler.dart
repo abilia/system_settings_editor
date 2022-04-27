7f2a448665bf751e9777c6ec9539f697d37c015c
rickard <rickardeden@gmail.com>
2022-04-27 10:08:58 +0200

    bumping version changing initialization

--- lib/tts/tts_handler.dart
+++ lib/tts/tts_handler.dart
@@ -1,12 +1,14 @@
 import 'package:acapela_tts/acapela_tts.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:seagull/config.dart';
+import 'package:seagull/logging.dart';
 
 abstract class TtsInterface {
   static Future<TtsInterface> implementation() async {
     if (Config.isMPGO) return FlutterTtsHandler();
+    final _log = Logger((TtsInterface).toString());
     final acapela = AcapelaTtsHandler();
-    await acapela.setLicense(
+    bool initialized = await acapela.setLicense(
       0x31364e69,
       0x004dfba3,
       '"4877 0 iN61 #EVALUATION#Abilia-Solna-Sweden"\n'
@@ -14,6 +16,13 @@ abstract class TtsInterface {
       'X6XAIrmYSRSUMSSNL25d7kHMXTKDS@Nlg2kl@YK4RsFVGPDX\n'
       'TqUDZO3UZgZhyJFRbfKSpQ##\n',
     );
+    List<Object?> voices = await acapela.availableVoices;
+    if (voices.isNotEmpty) {
+      await acapela.setVoice(voices.first.toString());
+    } else {
+      _log.warning('No acapela voices available');
+    }
+    _log.fine('Acapela initialized $initialized');
     return acapela;
   }
 
