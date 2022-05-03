f58deae7fa7251da7db3070b0231b38e1c31eb30
nikola <nmitrovic470@gmail.com>
2022-05-03 14:53:16 +0200

    SGC-1282 unnecessary import removed

--- test/ui/components/abilia_numpad_test.dart
+++ test/ui/components/abilia_numpad_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:seagull/bloc/all.dart';
 import 'package:seagull/ui/all.dart';
-import 'package:seagull/ui/components/abilia_num_pad.dart';
-
 import '../../fakes/all.dart';
 
 void main() {
