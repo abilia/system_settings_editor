a17921a7141a79398efafd30668182ef17603ba0
Daniel Duvanå <36152732+Daniel-BD@users.noreply.github.com>
2022-05-09 16:41:06 +0200

    Moved widget into edit timer page

--- lib/ui/pages/timer/edit_timer_page.dart
+++ lib/ui/pages/timer/edit_timer_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/services.dart';
 import 'package:seagull/bloc/all.dart';
 import 'package:seagull/ui/all.dart';
+import 'package:seagull/utils/all.dart';
 
 class EditTimerPage extends StatelessWidget {
   const EditTimerPage({Key? key}) : super(key: key);
@@ -26,7 +27,7 @@ class EditTimerPage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const TimerNamePictureAndTimeWidget(),
+                const _TimerInfoInput(),
                 Expanded(
                   child: TimerWheel.interactive(
                     lengthInSeconds: state.duration.inSeconds,
@@ -58,6 +59,86 @@ class EditTimerPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _TimerInfoInput extends StatelessWidget {
+  const _TimerInfoInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<EditTimerCubit, EditTimerState>(
+      builder: (context, state) {
+        return Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SelectPictureWidget(
+                  selectedImage: state.image,
+                  isLarge: true,
+                  onImageSelected: (selectedImage) {
+                    BlocProvider.of<EditTimerCubit>(context)
+                        .updateImage(selectedImage);
+                  },
+                ),
+                SizedBox(width: layout.formPadding.largeVerticalItemDistance),
+                Expanded(
+                  child: Column(
+                    children: [
+                      NameInput(
+                        key: TestKey.timerNameText,
+                        text: state.name,
+                        onEdit: (text) {
+                          if (state.name != text) {
+                            BlocProvider.of<EditTimerCubit>(context)
+                                .updateName(text);
+                          }
+                        },
+                        inputFormatters: [LengthLimitingTextInputFormatter(50)],
+                        inputHeading:
+                            Translator.of(context).translate.enterNameForTimer,
+                      ),
+                      SizedBox(height: layout.formPadding.verticalItemDistance),
+                      PickField(
+                        onTap: () async {
+                          final authProviders = copiedAuthProviders(context);
+                          final duration =
+                              await Navigator.of(context).push<Duration>(
+                            MaterialPageRoute(
+                              builder: (_) => MultiBlocProvider(
+                                providers: authProviders,
+                                child: EditTimerDurationPage(
+                                  initialDuration: state.duration,
+                                ),
+                              ),
+                            ),
+                          );
+                          if (duration != null) {
+                            context
+                                .read<EditTimerCubit>()
+                                .updateDuration(duration);
+                          }
+                        },
+                        leading: const Icon(AbiliaIcons.clock),
+                        text: Text(
+                          state.duration
+                              .toString()
+                              .split('.')
+                              .first
+                              .padLeft(8, '0'),
+                        ),
+                        trailing: PickField.trailingArrow,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
     );
   }
 }
