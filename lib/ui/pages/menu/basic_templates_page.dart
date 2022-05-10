df532758eb60f1194970d3dcab28c78189661001
Jonas Bornold <jonas@bornold.se>
2022-05-10 11:26:25 +0200

    SGC-1248 Fix sortOrder when creating new basic activity

--- lib/ui/pages/menu/basic_templates_page.dart
+++ lib/ui/pages/menu/basic_templates_page.dart
@@ -208,10 +208,9 @@ class AddTemplateButton extends StatelessWidget {
 
   void _addNewActivityTemplate(BuildContext context) {
     final authProviders = copiedAuthProviders(context);
-    final currentFolderId = context
-        .read<SortableArchiveCubit<BasicActivityData>>()
-        .state
-        .currentFolderId;
+    final sortableState =
+        context.read<SortableArchiveCubit<BasicActivityData>>().state;
+
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (_) => MultiBlocProvider(
@@ -231,7 +230,11 @@ class AddTemplateButton extends StatelessWidget {
               create: (context) => TemplateActivityWizardCubit(
                 editActivityCubit: context.read<EditActivityCubit>(),
                 sortableBloc: context.read<SortableBloc>(),
-                folderId: currentFolderId,
+                original: Sortable.createNew(
+                  groupId: sortableState.currentFolderId,
+                  sortOrder: sortableState.currentFolderSorted.firstSortOrder(),
+                  data: BasicActivityDataItem.createNew(),
+                ),
               ),
             ),
           ],
