42db108ffe04cfba7be764a34bbe5445056b4059
Jonas Bornold <jonas@bornold.se>
2022-05-18 19:10:17 +0200

    SGC-1337 Removing fullscreen permission from memoplanner

--- lib/bloc/permission/permission_cubit.dart
+++ lib/bloc/permission/permission_cubit.dart
@@ -6,6 +6,7 @@ import 'package:collection/collection.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:seagull/config.dart';
 
 import 'package:seagull/logging.dart';
 import 'package:seagull/utils/all.dart';
@@ -39,7 +40,7 @@ class PermissionCubit extends Cubit<PermissionState> with Info {
     {
       Permission.notification,
       Permission.microphone,
-      if (!Platform.isIOS) ...{
+      if (Config.isMPGO && !Platform.isIOS) ...{
         Permission.systemAlertWindow,
       },
       if (!Platform.isAndroid) ...{
