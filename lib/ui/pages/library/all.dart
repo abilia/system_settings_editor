1b502ce5732520872d046ac2039af0dbb2247fcb
Jonas Bornold <jonas@bornold.se>
2022-05-18 15:49:10 +0200

    SGC-1535 Remove unused code

--- lib/ui/pages/library/all.dart
+++ lib/ui/pages/library/all.dart
@@ -1,5 +1,2 @@
 export 'library_page.dart';
 export 'list_library.dart';
-export 'basic_library_item.dart';
-export '../edit_activity/info_items/note_library_page.dart';
-export '../edit_activity/info_items/checklist_library_page.dart';
