5772edee02e838a9ee6dede1b69e4c071f1bbb0e
nikola <nmitrovic470@gmail.com>
2022-06-10 11:22:38 +0200

    SGC-1672 renaming

--- lib/ui/themes/layouts/components/all.dart
+++ lib/ui/themes/layouts/components/all.dart
@@ -2,5 +2,5 @@ export 'app_bar_layout.dart';
 export 'clock_layout.dart';
 export 'action_button_layout.dart';
 export 'font_size_layout.dart';
-export 'month_preview_layout_large.dart';
-export 'month_calendar_layout_large.dart';
+export 'month_preview_layout.dart';
+export 'month_calendar_layout.dart';
