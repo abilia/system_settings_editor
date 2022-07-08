b9a94346022a6fc93179244d547b09433a01f73d
Daniel Duvanå <36152732+Daniel-BD@users.noreply.github.com>
2022-07-08 13:34:58 +0200

    renaming function

--- lib/ui/pages/edit_activity/info_items/edit_note_page.dart
+++ lib/ui/pages/edit_activity/info_items/edit_note_page.dart
@@ -74,7 +74,7 @@ class _EditNotePageState extends State<EditNotePage> {
               LayoutBuilder(
                 builder: (context, constraints) {
                   final textRenderSize =
-                      _textEditingController.text.calulcateTextRenderSize(
+                      _textEditingController.text.calculateTextRenderSize(
                     constraints: constraints,
                     textStyle:
                         Theme.of(context).textTheme.bodyText1 ?? bodyText1,
