7fd5c06f92d871678ed7dcf2408e97aa269246fb
Jonas Bornold <jonas@bornold.se>
2022-07-25 10:17:22 +0200

    Revert "SGC-1797 button background non-transparent"
    This reverts commit 36117716147ff6cae8ca3e57687937bcc1384aa0.

--- lib/ui/themes/button_styles.dart
+++ lib/ui/themes/button_styles.dart
@@ -213,9 +213,9 @@ final actionButtonStyleDark = _actionButtonStyle.copyWith(
         return Colors.transparent;
       }
       if (states.contains(MaterialState.pressed)) {
-        return AbiliaColors.white140;
+        return AbiliaColors.transparentBlack40;
       }
-      return AbiliaColors.white120;
+      return AbiliaColors.transparentBlack20;
     },
   ),
   shape: MaterialStateProperty.resolveWith((Set<MaterialState> states) {
