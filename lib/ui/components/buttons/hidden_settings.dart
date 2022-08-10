18797583fa0f05a3911593db896ded2c28cd273a
Daniel Duvanå <36152732+Daniel-BD@users.noreply.github.com>
2022-08-10 13:11:53 +0200

    Moved HiddenSetting buttons...
    so it's always underneath the categories buttons, or if they are hidden, in the place the categories buttons would normally be.

--- lib/ui/components/buttons/hidden_settings.dart
+++ lib/ui/components/buttons/hidden_settings.dart
@@ -3,8 +3,7 @@ import 'package:seagull/ui/all.dart';
 import 'package:seagull/utils/all.dart';
 
 class HiddenSetting extends StatefulWidget {
-  const HiddenSetting(this.showCategories, {Key? key}) : super(key: key);
-  final bool showCategories;
+  const HiddenSetting({Key? key}) : super(key: key);
 
   @override
   _HiddenSettingState createState() => _HiddenSettingState();
@@ -14,68 +13,60 @@ class _HiddenSettingState extends State<HiddenSetting> {
   bool rightTapped = false, leftTapped = false;
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topLeft,
-      child: Padding(
-        padding: EdgeInsets.only(
-          top: widget.showCategories ? layout.category.height : 0,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        GestureDetector(
+          key: TestKey.hiddenSettingsButtonLeft,
+          behavior: HitTestBehavior.translucent,
+          onTap: () async {
+            if (!leftTapped) {
+              leftTapped = true;
+              rightTapped = false;
+            } else if (rightTapped) {
+              leftTapped = false;
+              rightTapped = false;
+              final navigator = Navigator.of(context);
+              final authProviders = copiedAuthProviders(context);
+              final accessGranted = await codeProtectAccess(
+                context,
+                restricted: (codeSettings) => codeSettings.protectSettings,
+                name: Translator.of(context).translate.settings,
+              );
+              if (accessGranted) {
+                navigator.push(MaterialPageRoute(
+                  builder: (_) => MultiBlocProvider(
+                    providers: authProviders,
+                    child: const SettingsPage(),
+                  ),
+                  settings:
+                      const RouteSettings(name: 'SettingsPage from hidden'),
+                ));
+              }
+            }
+          },
+          child: SizedBox(
+            width: layout.actionButton.size,
+            height: layout.actionButton.size,
+          ),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            GestureDetector(
-              key: TestKey.hiddenSettingsButtonLeft,
-              behavior: HitTestBehavior.translucent,
-              onTap: () async {
-                if (!leftTapped) {
-                  leftTapped = true;
-                  rightTapped = false;
-                } else if (rightTapped) {
-                  leftTapped = false;
-                  rightTapped = false;
-                  final navigator = Navigator.of(context);
-                  final authProviders = copiedAuthProviders(context);
-                  final accessGranted = await codeProtectAccess(
-                    context,
-                    restricted: (codeSettings) => codeSettings.protectSettings,
-                    name: Translator.of(context).translate.settings,
-                  );
-                  if (accessGranted) {
-                    navigator.push(MaterialPageRoute(
-                      builder: (_) => MultiBlocProvider(
-                        providers: authProviders,
-                        child: const SettingsPage(),
-                      ),
-                      settings:
-                          const RouteSettings(name: 'SettingsPage from hidden'),
-                    ));
-                  }
-                }
-              },
-              child: SizedBox(
-                width: layout.actionButton.size,
-                height: layout.actionButton.size,
-              ),
-            ),
-            GestureDetector(
-              key: TestKey.hiddenSettingsButtonRight,
-              behavior: HitTestBehavior.translucent,
-              onTap: () {
-                if (leftTapped && !rightTapped) {
-                  rightTapped = true;
-                } else if (rightTapped) {
-                  leftTapped = false;
-                  rightTapped = false;
-                }
-              },
-              child: SizedBox(
-                width: layout.actionButton.size,
-                height: layout.actionButton.size,
-              ),
-            ),
-          ],
+        GestureDetector(
+          key: TestKey.hiddenSettingsButtonRight,
+          behavior: HitTestBehavior.translucent,
+          onTap: () {
+            if (leftTapped && !rightTapped) {
+              rightTapped = true;
+            } else if (rightTapped) {
+              leftTapped = false;
+              rightTapped = false;
+            }
+          },
+          child: SizedBox(
+            width: layout.actionButton.size,
+            height: layout.actionButton.size,
+          ),
         ),
-      ),
+      ],
     );
   }
 }
