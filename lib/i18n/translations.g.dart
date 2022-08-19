39629b090a5a44b7c3c98dfba0214c9b7ffc277a
Tobias Junsten <tobias@junsten.se>
2022-08-19 15:07:22 +0200

    SGC-1885: cherry pick from release branch

--- lib/i18n/translations.g.dart
+++ lib/i18n/translations.g.dart
@@ -7760,10 +7760,10 @@ class NB extends Translated {
   final String timerDelete = 'Vil du slette timeren?';
 
   @override
-  final String timerPause = 'Vil du sette tidtakeren på pause?';
+  final String timerPause = 'Vil du sette timeren på pause?';
 
   @override
-  final String timerResume = 'Vil du fortsette tidtakeren?';
+  final String timerResume = 'Vil du fortsette timeren?';
 
   @override
   final String timerPaused = 'Pauset';
@@ -7877,7 +7877,7 @@ class NB extends Translated {
   final String selectTemplateTimer = 'Velg timere';
 
   @override
-  final String timer = 'Tidtaker';
+  final String timer = 'Timer';
 
   @override
   final String timeIsUp = 'Tiden er slutt';
