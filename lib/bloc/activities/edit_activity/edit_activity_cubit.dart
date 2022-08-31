716c2ac4ba0141ba4533bcb35b5d54ec71953403
Daniel Lindgren <danel.lindgren@gmail.com>
2022-08-31 10:46:00 +0200

    Fix issue where recurring days would be reset on changing end date

--- lib/bloc/activities/edit_activity/edit_activity_cubit.dart
+++ lib/bloc/activities/edit_activity/edit_activity_cubit.dart
@@ -183,12 +183,15 @@ class EditActivityCubit extends Cubit<EditActivityState> {
     final type = newType ?? state.activity.recurs.recurrance;
     final endDate = newEndDate ??
         (state.activity.isRecurring ? state.timeInterval.endDate : null);
+    final recurs = endDate != null && newType == null
+        ? state.activity.recurs.changeEnd(endDate)
+        : _newRecurs(
+            type,
+            state.timeInterval.startDate,
+            endDate,
+          );
     changeRecurrence(
-      _newRecurs(
-        type,
-        state.timeInterval.startDate,
-        endDate,
-      ),
+      recurs,
       timeInterval: state.timeInterval.changeEndDate(
         newType == RecurrentType.yearly ? Recurs.noEndDate : endDate,
       ),
