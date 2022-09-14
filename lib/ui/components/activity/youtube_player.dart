55495660640af0c0cd32aef200683218112e33e4
Jonas Bornold <jonas@bornold.se>
2022-09-14 11:47:09 +0200

    SGC-1931 Fixing YouTube player

--- lib/ui/components/activity/youtube_player.dart
+++ lib/ui/components/activity/youtube_player.dart
@@ -1,5 +1,7 @@
+import 'package:seagull/db/all.dart';
+import 'package:seagull/getit.dart';
 import 'package:seagull/ui/all.dart';
-import 'package:youtube_player_iframe/youtube_player_iframe.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart' as yt;
 
 class YoutubePlayer extends StatefulWidget {
   final String url;
@@ -11,29 +13,36 @@ class YoutubePlayer extends StatefulWidget {
 }
 
 class _YoutubePlayerState extends State<YoutubePlayer> {
-  late final YoutubePlayerController _controller;
+  late final yt.YoutubePlayerController _controller;
 
   @override
   void initState() {
     super.initState();
-    final url =
-        widget.url.startsWith('http') ? widget.url : 'https://${widget.url}';
-    final videoId = YoutubePlayerController.convertUrlToId(url);
-    _controller = YoutubePlayerController.fromVideoId(
+
+    final videoId = yt.YoutubePlayerController.convertUrlToId(
+      widget.url.startsWith('http') ? widget.url : 'https://${widget.url}',
+    );
+    _controller = yt.YoutubePlayerController.fromVideoId(
       videoId: videoId ?? '',
-      params: const YoutubePlayerParams(
+      params: yt.YoutubePlayerParams(
         strictRelatedVideos: true,
-        showFullscreenButton: true,
+        showVideoAnnotations: false,
+        interfaceLanguage: GetIt.I<SettingsDb>().language,
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return YoutubePlayerScaffold(
-      builder: (context, player) => player,
-      controller: _controller,
-      aspectRatio: 3,
+    return MediaQuery(
+      // As of youtube_player_iframe 3.0.4 the player fixes it's aspectratio to
+      // MediaQuery.of(context).size.aspectRatio
+      // if orientation == Orientation.landscape
+      // So this ugly hack for forcing correct aspect ration
+      // Please someone in the future, just replace this stupid plugin with
+      // an ordinary WebView
+      data: MediaQuery.of(context).copyWith(size: const Size(16, 9)),
+      child: yt.YoutubePlayer(controller: _controller),
     );
   }
 }
