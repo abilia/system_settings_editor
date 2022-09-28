70e9cbc0bbd07939930b6ecd396c036a3f8bcb95
SlowDeepCoder <danel.lindgren@gmail.com>
2022-09-28 16:47:58 +0200

    Fix failing test

--- lib/ui/components/buttons/play_alarm_speech_button.dart
+++ lib/ui/components/buttons/play_alarm_speech_button.dart
@@ -14,34 +14,33 @@ class PlayAlarmSpeechButton extends StatelessWidget {
     Key? key,
   }) : super(key: key);
   @override
-  Widget build(BuildContext context) {
-    final settings = context.watch<MemoplannerSettingsBloc>().state;
-    return BlocProvider(
-      create: (context) => SoundCubit(
-        storage: GetIt.I<FileStorage>(),
-        userFileCubit: context.read<UserFileCubit>(),
-      ),
-      child: Row(
-        children: [
-          PlaySoundButton(sound: alarm.speech),
-          if (settings is MemoplannerSettingsNotLoaded)
-            const SizedBox.shrink()
-          else
-            BlocProvider(
-              create: (context) => AlarmSpeechCubit(
-                alarm: alarm,
-                now: () => GetIt.I<Ticker>().time,
-                alarmSettings: settings.alarm,
-                soundCubit: context.read<SoundCubit>(),
-                touchStream: context.read<TouchDetectionCubit>().stream,
-                selectedNotificationStream:
-                    Config.isMPGO ? selectNotificationSubject : null,
-              ),
-              lazy: false,
-              child: const SizedBox.shrink(),
+  Widget build(BuildContext context) => BlocProvider(
+        create: (context) => SoundCubit(
+          storage: GetIt.I<FileStorage>(),
+          userFileCubit: context.read<UserFileCubit>(),
+        ),
+        child: Row(
+          children: [
+            PlaySoundButton(sound: alarm.speech),
+            BlocBuilder<MemoplannerSettingsBloc, MemoplannerSettings>(
+              builder: (context, settings) => settings
+                      is MemoplannerSettingsNotLoaded
+                  ? const SizedBox.shrink()
+                  : BlocProvider(
+                      create: (context) => AlarmSpeechCubit(
+                        alarm: alarm,
+                        now: () => GetIt.I<Ticker>().time,
+                        alarmSettings: settings.alarm,
+                        soundCubit: context.read<SoundCubit>(),
+                        touchStream: context.read<TouchDetectionCubit>().stream,
+                        selectedNotificationStream:
+                            Config.isMPGO ? selectNotificationSubject : null,
+                      ),
+                      lazy: false,
+                      child: const SizedBox.shrink(),
+                    ),
             ),
-        ],
-      ),
-    );
-  }
+          ],
+        ),
+      );
 }
