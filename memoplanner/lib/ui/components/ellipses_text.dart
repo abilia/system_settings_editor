e4e4aa2e9a024fddf89c624196cd7432ac5b80ae
Daniel Duvanå <36152732+Daniel-BD@users.noreply.github.com>
2022-11-11 15:41:00 +0100

    Addded comment

--- memoplanner/lib/ui/components/ellipses_text.dart
+++ memoplanner/lib/ui/components/ellipses_text.dart
@@ -1,5 +1,12 @@
 import 'package:memoplanner/ui/all.dart';
 
+// In order for TextOverflow.ellipses to show the ellipses on a Text,
+// the provided maxLines value can't be bigger than the number of lines
+// that can fit in the parent Widget. If for example 3 lines of text
+// can fit but maxLines is set 4, then instead of ending the third line of text
+// with an ellipses the text is clipped. This widget solves this by calculating
+// the max number of lines that can fit and clamps the given maxLines to
+// make sure ellipses is always shown if text overflows.
 class EllipsesText extends StatelessWidget {
   const EllipsesText(
     this.data, {
