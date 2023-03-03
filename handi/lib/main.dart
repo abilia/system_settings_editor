fff3092f16986ddcf322f160c4e04ed0e8b069e9
Jonas Bornold <jonas@bornold.se>
2023-03-03 17:40:06 +0100

    SGC-2282 handi login

--- handi/lib/main.dart
+++ handi/lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:auth/bloc/all.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:handi/ui/pages/logged_in_page.dart';
+import 'package:handi/ui/pages/login_page.dart';
+import 'package:seagull_analytics/seagull_analytics.dart';
 
 void main() {
   runApp(
@@ -6,15 +11,51 @@ void main() {
   );
 }
 
+final _navigatorKey = GlobalKey<NavigatorState>();
+
 class HandiApp extends StatelessWidget {
   const HandiApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Handi!'),
+    return BlocListener<AuthenticationBloc, AuthenticationState>(
+      listenWhen: (previous, current) =>
+          previous.runtimeType != current.runtimeType ||
+          previous.forcedNewState != current.forcedNewState,
+      listener: (context, state) async {
+        final navigator = _navigatorKey.currentState;
+        if (navigator == null) {
+          context.read<AuthenticationBloc>().add(NotReady());
+          return;
+        }
+        if (state is Authenticated) {
+          await navigator.pushAndRemoveUntil<void>(
+            MaterialPageRoute<void>(
+              builder: (_) => const LoggedInPage(),
+              settings: (LoggedInPage).routeSetting(),
+            ),
+            (_) => false,
+          );
+        } else if (state is Unauthenticated) {
+          await navigator.pushAndRemoveUntil<void>(
+            MaterialPageRoute<void>(
+              builder: (_) => const LoginPage(),
+              settings: (LoginPage).routeSetting(
+                properties: {
+                  'logout reason': state.loggedOutReason.name,
+                },
+              ),
+            ),
+            (_) => false,
+          );
+        }
+      },
+      child: MaterialApp(
+        navigatorKey: _navigatorKey,
+        home: const Scaffold(
+          body: Center(
+            child: Text('Handi!'),
+          ),
         ),
       ),
     );
