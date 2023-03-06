f2189ff0315bbd93059d88ae48baa2347d8c299c
Daniel Lindgren <danel.lindgren@gmail.com>
2023-03-06 14:15:50 +0100

    add github actions for packages and att tests for seagull_analytics

--- memoplanner/test/bloc/timer/timer_cubit_test.dart
+++ memoplanner/test/bloc/timer/timer_cubit_test.dart
@@ -1,11 +1,10 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:memoplanner/bloc/all.dart';
+import 'package:memoplanner/models/all.dart';
 import 'package:memoplanner/repository/all.dart';
 import 'package:test/test.dart';
 
-import 'package:memoplanner/models/all.dart';
-
-import '../../fakes/fake_seagull_analytics.dart';
+import '../../fakes/all.dart';
 import '../../mocks/mocks.dart';
 
 void main() {
