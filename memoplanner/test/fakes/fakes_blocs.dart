cb378939e1003b680e781e1a5cd71a3e2de90ad7
Daniel Lindgren <danel.lindgren@gmail.com>
2023-04-12 18:52:11 +0200

    add FakeScrollPositionCubit to failing test

--- memoplanner/test/fakes/fakes_blocs.dart
+++ memoplanner/test/fakes/fakes_blocs.dart
@@ -1,4 +1,5 @@
 import 'package:collection/src/unmodifiable_wrappers.dart';
+import 'package:flutter/material.dart';
 import 'package:memoplanner/bloc/all.dart';
 import 'package:memoplanner/models/all.dart';
 import 'package:memoplanner/repository/all.dart';
@@ -9,10 +10,13 @@ import 'all.dart';
 class FakeSyncBloc extends Fake implements SyncBloc {
   @override
   Stream<Synced> get stream => const Stream.empty();
+
   @override
   void add(SyncEvent event) {}
+
   @override
   Future<bool> hasDirty() => Future.value(false);
+
   @override
   Future<void> close() async {}
 }
@@ -20,8 +24,10 @@ class FakeSyncBloc extends Fake implements SyncBloc {
 class FakeAuthenticationBloc extends Fake implements AuthenticationBloc {
   @override
   Stream<AuthenticationState> get stream => const Stream.empty();
+
   @override
   AuthenticationState get state => const Authenticated(user: Fakes.user);
+
   @override
   Future<void> close() async {}
 }
@@ -29,8 +35,10 @@ class FakeAuthenticationBloc extends Fake implements AuthenticationBloc {
 class FakeSortableBloc extends Fake implements SortableBloc {
   @override
   Stream<SortableState> get stream => const Stream.empty();
+
   @override
   SortableState get state => SortablesNotLoaded();
+
   @override
   Future<void> close() async {}
 }
@@ -38,6 +46,7 @@ class FakeSortableBloc extends Fake implements SortableBloc {
 class FakeGenericCubit extends Fake implements GenericCubit {
   @override
   Stream<GenericState> get stream => const Stream.empty();
+
   @override
   Future<void> close() async {}
 }
@@ -45,15 +54,20 @@ class FakeGenericCubit extends Fake implements GenericCubit {
 class FakeActivitiesBloc extends Fake implements ActivitiesBloc {
   @override
   late final ActivityRepository activityRepository;
+
   FakeActivitiesBloc({ActivityRepository? activityRepository}) {
     this.activityRepository = activityRepository ?? FakeActivityRepository();
   }
+
   @override
   Stream<ActivitiesChanged> get stream => const Stream.empty();
+
   @override
   ActivitiesChanged get state => ActivitiesChanged();
+
   @override
   void add(ActivitiesEvent event) {}
+
   @override
   Future<void> close() async {}
 }
@@ -62,9 +76,11 @@ class FakeMemoplannerSettingsBloc extends Fake
     implements MemoplannerSettingsBloc {
   @override
   Stream<MemoplannerSettings> get stream => const Stream.empty();
+
   @override
   MemoplannerSettings get state =>
       MemoplannerSettingsLoaded(const MemoplannerSettings());
+
   @override
   Future<void> close() async {}
 }
@@ -85,6 +101,7 @@ class FakeTimepillarCubit extends Fake implements TimepillarCubit {
         showNightCalendar: false,
         day: DateTime(1066, 10, 14),
       );
+
   @override
   Future<void> close() async {}
 }
@@ -102,6 +119,7 @@ class FakeTimepillarMeasuresCubit extends Fake
         ),
         1,
       );
+
   @override
   Future<void> close() async {}
 }
@@ -117,6 +135,7 @@ class FakeUserFileBloc extends Fake implements UserFileBloc {
 class FakeDayPickerBloc extends Fake implements DayPickerBloc {
   @override
   Stream<DayPickerState> get stream => const Stream.empty();
+
   @override
   Future<void> close() async {}
 }
@@ -124,6 +143,7 @@ class FakeDayPickerBloc extends Fake implements DayPickerBloc {
 class FakeDayEventsCubit extends Fake implements DayEventsCubit {
   @override
   Stream<EventsState> get stream => const Stream.empty();
+
   @override
   Future<void> close() async {}
 }
@@ -131,6 +151,7 @@ class FakeDayEventsCubit extends Fake implements DayEventsCubit {
 class FakeAlarmCubit extends Fake implements AlarmCubit {
   @override
   Stream<NotificationAlarm?> get stream => const Stream.empty();
+
   @override
   Future<void> close() async {}
 }
@@ -138,6 +159,7 @@ class FakeAlarmCubit extends Fake implements AlarmCubit {
 class FakeCalendarViewBloc extends Fake implements CalendarViewCubit {
   @override
   Stream<CalendarViewState> get stream => const Stream.empty();
+
   @override
   Future<void> close() async {}
 }
@@ -145,10 +167,13 @@ class FakeCalendarViewBloc extends Fake implements CalendarViewCubit {
 class FakeLicenseCubit extends Fake implements LicenseCubit {
   @override
   bool get validLicense => true;
+
   @override
   ValidLicense get state => ValidLicense();
+
   @override
   Stream<LicenseState> get stream => const Stream.empty();
+
   @override
   Future<void> close() async {}
 }
@@ -156,6 +181,7 @@ class FakeLicenseCubit extends Fake implements LicenseCubit {
 class FakeEditActivityCubit extends Fake implements EditActivityCubit {
   @override
   Stream<EditActivityState> get stream => const Stream.empty();
+
   @override
   Future<void> close() async {}
 }
@@ -275,4 +301,20 @@ class FakeScrollPositionCubit extends Fake implements ScrollPositionCubit {
 
   @override
   Future<void> close() async {}
+
+  @override
+  void updateState({
+    required ScrollController scrollController,
+    required double nowOffset,
+    required double inViewMargin,
+  }) {}
+
+  @override
+  Future<void> goToNow({
+    Duration duration = Duration.zero,
+    Curve curve = Curves.easeInOut,
+  }) async {}
+
+  @override
+  void reset() {}
 }
