d63e4d51984b92fb9a002cb6b7d2eacaccdeabe9
Jonas Bornold <jonas@bornold.se>
2023-05-11 12:29:53 +0200

    SGC-2456 Better test description

--- packages/auth/test/bloc/authentication/authentication_bloc_test.dart
+++ packages/auth/test/bloc/authentication/authentication_bloc_test.dart
@@ -157,7 +157,8 @@ void main() {
     );
 
     blocTest(
-      'logged out cancel all on logout and repo in order',
+      'logged out cancel callback after logout on repository,'
+      ' or the token is removed before we can delete the token in the backend',
       setUp: () => when(() => mockedUserRepository.me())
           .thenAnswer((_) => Future.error(UnauthorizedException())),
       build: () => AuthenticationBloc(
