069c037e851241f24777e6bf2b41518852030b10
rickard <rickardeden@gmail.com>
2023-06-28 10:29:42 +0200

    SGC-2507 missing files

---
+++ handi/lib/l10n/generated/intl/messages_sv.dart
@@ -0,0 +1,47 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a sv locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = MessageLookup();
+
+typedef String? MessageIfAbsent(String? messageStr, List<Object>? args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  @override
+  String get localeName => 'sv';
+
+  @override
+  final Map<String, dynamic> messages =
+      _notInlinedMessages(_notInlinedMessages);
+
+  static Map<String, dynamic> _notInlinedMessages(_) => {
+        'generics': MessageLookupByLibrary.simpleMessage(''),
+        'logOut': MessageLookupByLibrary.simpleMessage('Logga ut'),
+        'loggedOut':
+            MessageLookupByLibrary.simpleMessage('Du har blivit utloggad'),
+        'ok': MessageLookupByLibrary.simpleMessage(''),
+        'password': MessageLookupByLibrary.simpleMessage('Lösenord'),
+        'pushReceived': MessageLookupByLibrary.simpleMessage('Push mottagen'),
+        'signIn': MessageLookupByLibrary.simpleMessage('Logga in'),
+        'sortables': MessageLookupByLibrary.simpleMessage(''),
+        'supportPersons':
+            MessageLookupByLibrary.simpleMessage('Supportpersoner'),
+        'sync': MessageLookupByLibrary.simpleMessage('Synkronisera'),
+        'textToSpeech': MessageLookupByLibrary.simpleMessage('Text till tal'),
+        'unauthorized': MessageLookupByLibrary.simpleMessage('Obehörig'),
+        'upcomingActivities':
+            MessageLookupByLibrary.simpleMessage('Kommande aktiviteter'),
+        'userFiles': MessageLookupByLibrary.simpleMessage('Användarfiler'),
+        'userName': MessageLookupByLibrary.simpleMessage('Användarnamn')
+      };
+}
---
+++ handi/lib/l10n/generated/l10n.dart
@@ -0,0 +1,240 @@
+// GENERATED CODE
+//
+// After the template files .arb have been changed,
+// generate this class by the command in the terminal:
+// flutter pub run lokalise_flutter_sdk:gen-lok-l10n
+//
+// Please see https://pub.dev/packages/lokalise_flutter_sdk
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
+import 'package:lokalise_flutter_sdk/lokalise_flutter_sdk.dart';
+import 'intl/messages_all.dart';
+
+class Lt {
+  Lt._internal();
+
+  static const LocalizationsDelegate<Lt> delegate = _AppLocalizationDelegate();
+
+  static const List<Locale> supportedLocales = [
+    Locale.fromSubtags(languageCode: 'en'),
+    Locale.fromSubtags(languageCode: 'sv')
+  ];
+
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
+      <LocalizationsDelegate<dynamic>>[
+    delegate,
+    GlobalMaterialLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+  ];
+
+  static final Map<String, List<String>> _metadata = {
+    'generics': [],
+    'logOut': [],
+    'loggedOut': [],
+    'ok': [],
+    'password': [],
+    'pushReceived': [],
+    'signIn': [],
+    'sortables': [],
+    'supportPersons': [],
+    'sync': [],
+    'textToSpeech': [],
+    'unauthorized': [],
+    'userFiles': [],
+    'userName': [],
+    'upcomingActivities': []
+  };
+
+  static Future<Lt> load(Locale locale) {
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
+    Lokalise.instance.metadata = _metadata;
+
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      final instance = Lt._internal();
+      return instance;
+    });
+  }
+
+  static Lt of(BuildContext context) {
+    final instance = Localizations.of<Lt>(context, Lt);
+    assert(instance != null,
+        'No instance of Lt present in the widget tree. Did you add Lt.delegate in localizationsDelegates?');
+    return instance!;
+  }
+
+  /// `Generics`
+  String get generics {
+    return Intl.message(
+      'Generics',
+      name: 'generics',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Log out`
+  String get logOut {
+    return Intl.message(
+      'Log out',
+      name: 'logOut',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `You have been logged out.`
+  String get loggedOut {
+    return Intl.message(
+      'You have been logged out.',
+      name: 'loggedOut',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Ok`
+  String get ok {
+    return Intl.message(
+      'Ok',
+      name: 'ok',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Password`
+  String get password {
+    return Intl.message(
+      'Password',
+      name: 'password',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Push received`
+  String get pushReceived {
+    return Intl.message(
+      'Push received',
+      name: 'pushReceived',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Sign In`
+  String get signIn {
+    return Intl.message(
+      'Sign In',
+      name: 'signIn',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Sortables`
+  String get sortables {
+    return Intl.message(
+      'Sortables',
+      name: 'sortables',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Support persons`
+  String get supportPersons {
+    return Intl.message(
+      'Support persons',
+      name: 'supportPersons',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Sync`
+  String get sync {
+    return Intl.message(
+      'Sync',
+      name: 'sync',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Text to speech`
+  String get textToSpeech {
+    return Intl.message(
+      'Text to speech',
+      name: 'textToSpeech',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Unauthorized`
+  String get unauthorized {
+    return Intl.message(
+      'Unauthorized',
+      name: 'unauthorized',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `User Files`
+  String get userFiles {
+    return Intl.message(
+      'User Files',
+      name: 'userFiles',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `User Name`
+  String get userName {
+    return Intl.message(
+      'User Name',
+      name: 'userName',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Upcoming activities`
+  String get upcomingActivities {
+    return Intl.message(
+      'Upcoming activities',
+      name: 'upcomingActivities',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class _AppLocalizationDelegate extends LocalizationsDelegate<Lt> {
+  const _AppLocalizationDelegate();
+
+  @override
+  bool isSupported(Locale locale) => Lt.supportedLocales.any(
+      (supportedLocale) => supportedLocale.languageCode == locale.languageCode);
+
+  @override
+  Future<Lt> load(Locale locale) => Lt.load(locale);
+
+  @override
+  bool shouldReload(_AppLocalizationDelegate old) => false;
+}
