6bf0b483801f8f3c2c9af08ef95eef49f6f132fd
Daniel Lindgren <danel.lindgren@gmail.com>
2023-09-12 12:57:38 +0200

    fix padding issues for checklist

--- memoplanner/lib/ui/components/activity/activity_info.dart
+++ memoplanner/lib/ui/components/activity/activity_info.dart
@@ -188,7 +188,6 @@ class Attachment extends StatelessWidget with ActivityMixin {
       return ChecklistView(
         item,
         day: activityDay.day,
-        padding: layout.templates.s2,
         onTap: (question) async {
           final signedOff = item.signOff(question, activityDay.day);
           final updatedActivity = activity.copyWith(
