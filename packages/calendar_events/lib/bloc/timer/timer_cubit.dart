239d323bb0e27885da887831716ec9e29d80004e
Daniel Lindgren <danel.lindgren@gmail.com>
2023-10-06 14:45:57 +0200

    SGC-2615: remove ticker from timer cubit

--- packages/calendar_events/lib/bloc/timer/timer_cubit.dart
+++ packages/calendar_events/lib/bloc/timer/timer_cubit.dart
@@ -5,18 +5,15 @@ import 'package:bloc/bloc.dart';
 import 'package:calendar_events/calendar_events.dart';
 import 'package:equatable/equatable.dart';
 import 'package:seagull_analytics/seagull_analytics.dart';
-import 'package:seagull_clock/ticker.dart';
 
 part 'timer_state.dart';
 
 class TimerCubit extends Cubit<TimerState> {
   final TimerDb timerDb;
-  final Ticker ticker;
   final SeagullAnalytics analytics;
 
   TimerCubit({
     required this.timerDb,
-    required this.ticker,
     required this.analytics,
   }) : super(TimerState());
 
